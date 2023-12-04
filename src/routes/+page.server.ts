01322f49a235b5c813c6d2c78bf8ae9647b25ca5
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-04 17:44:42 -0300

    show readme from github with a fetch

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,27 @@
 import type { Load } from "@sveltejs/kit";
 
 import { compile } from 'mdsvex';
-import README from './../../README.md?raw';
 
 export const load: Load = async () => {
 
-	const compiledResponse = await compile(README, {});
+	try {
+		const response = await fetch('https://raw.githubusercontent.com/BrunoMollo/ZodActions/main/README.md')
+		const md = await response.text()
 
-	console.log('compiledResponse is: ', compiledResponse);
+		const compiledResponse = await compile(md, {});
 
-	if (!compiledResponse?.code) {
+		if (!compiledResponse?.code) {
+			return { content: '' }
+		}
+
+		const search = '{@html `<code class="language-bash"><span class="token function">npm</span> i zod-actions</code>`}'
+		const replace = '<code class="language-bash"><span class="token function">npm</span> i zod-actions</code>'
+		const parsed = compiledResponse.code.replace(search, replace)
+
+		return { content: parsed };
+
+	} catch {
 		return { content: '' }
 	}
 
-	const search = '{@html `<code class="language-bash"><span class="token function">npm</span> i zod-actions</code>`}'
-	const replace = '<code class="language-bash"><span class="token function">npm</span> i zod-actions</code>'
-	const parsed = compiledResponse.code.replace(search, replace)
-
-	return { content: parsed };
 };
