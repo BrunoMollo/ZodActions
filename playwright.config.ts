4170ed84a41cc30a7228a8d5f4ab83d5870d2b45
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-12-04 20:06:41 -0300

    changes in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,26 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices, type PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	fullyParallel: true,
+
+	use: {
+		trace: 'on-first-retry',
+	},
+
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		},
+	],
+
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+
 
 };
 
